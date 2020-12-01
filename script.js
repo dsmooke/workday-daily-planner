3fe16e036288118ed3eeebaed15b2181fe3cbf03
Danny Smooke <dana.smooke@gmail.com>
2020-12-01 16:05:39 -0500

    created and added external js file with initial code

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// var mySchedule = 
+
+
+//get data for header date [correct!]
+function getHeaderDate() {
+  var currentHeaderDate = moment().format('dddd, MMMM Do, YYYY');
+  $("#currentDay").text(currentHeaderDate);
+}
+
+// load today's date (header date) [correct]
+getHeaderDate();
