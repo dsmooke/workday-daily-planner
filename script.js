4ea91d55404bcab28c45f1c798c3398d8e7bdcb0
Danny Smooke <dana.smooke@gmail.com>
2020-12-02 16:24:48 -0500

    added for loop to get hours - still working on it

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 
+//mySchedule refers to the current day's schedule
+
+
 // var mySchedule = 
 
 
@@ -10,3 +13,19 @@ function getHeaderDate() {
 
 // load today's date (header date) [correct]
 getHeaderDate();
+
+// var hourBlock = $("#hour-9")
+// //use bootstrap class names in js - id that relates to current hour 
+// $("div").html(`<div ${hourBlock} class="row time-block"></div>`);
+
+// $("div").attr("class="axy"")
+
+let hour = moment().format("h a");
+for (let hour = 9; hour <= 19; hour++) {
+  console.log(hour);
+}
+
+
+// var startDay = hour(9);
+var currentTime = moment().hour();
+
