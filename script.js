b32bea3f592fa6a642f7934205b15c6c1c944fdd
Danny Smooke <dana.smooke@gmail.com>
2020-12-02 18:57:00 -0500

    created addedEvent object with key-value pair of hour-array and event; outlined some functions. Code works but it's not there yet

--- script.js
+++ script.js
@@ -1,53 +1,103 @@
-
-//mySchedule refers to the current day's schedule
-
-
-// var mySchedule = 
-
-
-//get data for header date [correct!]
-function getHeaderDate() {
-  var currentHeaderDate = moment().format('dddd, MMMM Do, YYYY');
-  $("#currentDay").text(currentHeaderDate);
+var addedEvent = {
+  hour: ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"],
+  addedEvent: $("input")
 }
 
-// load today's date (header date) [correct]
-getHeaderDate();
+var inputData = $("<input>").text
 
-const hour = moment().format("h a");
+var time = addedEvent.hour[0];
+var eventBlock = $("input")
 
-function getTime() {
-  // var currentTime = moment().hour(9);
-  for (let hour = 9; hour <= 19; hour++) {
-  // console.log(currentTime);
-  console.log(hour);
+var saveBtn = $(".saveBtn");
+var saveEvent = $(".saveBtn");
+
+  
+function saveSchedule() {
+  localStorage.setItem("savedSchedule");JSON.stringify(savedSchedule)
+
+
+  saveEvent.on("click", function(event) {
+  event.preventDefault();
+  localStorage.setItem(hour, addedEvent)
+  console.log(saveSchedule);
+  }
+  )
 }
-// let hour = i;
 
-// var hour = moment().hour();
-// let time = moment().format("h A");
+// const mySchedule = ['event1', 'event2', 'event3']
+
+for (let i = 0; i<= addedEvent.hour.length; i++) {
+  console.log(`You have ${addedEvent[i]} at ${time}`)
+  if (eventBlock === null) {
+    console.log(`You are free at ${time}!`);
+  }
 }
-getTime();
-
-let container = $(".container");
-
-let hour9Div = $(`<div class='row time-block'>
-  <div class='col-2 hour'>${hour}</div>
-  <textarea class="col-8 description" placeholder="add event"></textarea> 
-  <button class="col-2 saveBtn>Save</button>
-  </div>`);
-container.append(hour9Div);
-
-// var hourBlock = $("#hour-9")
-// //use bootstrap class names in js - id that relates to current hour 
-// $("div").html(`<div ${hourBlock} class="row time-block"></div>`);
-
-// $("div").attr("class="axy"")
 
 
 
 
 
-// var startDay = hour(9);
+
+function displaySchedule() {
+
+}
+//   //mySchedule refers to the current day's schedule
+// var Event1 = [];
+// var eventData = $("input")
+// $("input").text(`${Event1}`)
+
+// console.log(eventData);
+
+
 // var currentTime = moment().hour();
+// function displayAddedEvents(){};
+// for (i = 9; i <=19; i++)
+
+//   $(".container").append(Event1)
+
+//   let schedule = JSON.parse(localStorage.getItem("schedule"));
+
+
+
+
+
+// //get data for header date [correct!]
+// function getHeaderDate() {
+//   var currentHeaderDate = moment().format('dddd, MMMM Do, YYYY');
+//   $("#currentDay").text(currentHeaderDate);
+// }
+
+// // load today's date (header date) [correct]
+// getHeaderDate();
+
+// const hour = moment().format("h a");
+// // var addedEvent = $()
+
+// function getTime() {
+//   // var currentTime = moment().hour(9);
+//   for (let hour = 9; hour <= 19; hour++) {
+//   // console.log(currentTime);
+//   console.log(hour);
+// }
+// // let hour = i;
+
+// // var hour = moment().hour();
+// // let time = moment().format("h A");
+// }
+// getTime(9);
+
+// let container = $(".container");
+
+// let hour9Div = $(`<div class='row time-block'>
+//   <div class='col-2 hour'>${hour}</div>
+//   <textarea class="col-8 description" placeholder="add event"></textarea> 
+//   <button class="col-2 saveBtn>Save</button>
+//   </div>`);
+// container.append(hour9Div);
+
+
+
+
+
+
 
