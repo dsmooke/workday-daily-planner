03ab99081fab3a523a4589a898f53e12f195b7c0
Danny Smooke <dana.smooke@gmail.com>
2020-12-02 20:14:40 -0500

    added variable with condition that works, semi, but not individually. Need typed data to be inserted into event variable to be accessed by index...

--- script.js
+++ script.js
@@ -1,66 +1,102 @@
+// want to be able to compare hour with current time
 var addedEvent = {
   hour: ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"],
-  addedEvent: $("input")
+  event: "TEST"
 }
-
-//want typed info to display within input element
-var inputData = $("<input>").text
-
-//want time to be defined as the event's hour at indexNumber
-var time = addedEvent.hour[0];
-
-
 var eventBlock = $("input")
 
-var saveBtn = $(".saveBtn");
-var saveEvent = $(".saveBtn");
-
-// function to save newest schedule and store it
-function saveSchedule() {
-  localStorage.setItem("savedSchedule");JSON.stringify(savedSchedule)
-
-// on save button want to save event
-  saveEvent.on("click", function(event) {
-  event.preventDefault();
-  localStorage.setItem(hour, addedEvent)
-  console.log(saveSchedule);
-  }
-  )
-}
-
-// const mySchedule = ['event1', 'event2', 'event3']
+// var addedEvent = ['event1', 'event2', 'event3']
 
 // at each hour display 'you have ${input/typed event} at ${time}'
 for (let i = 0; i<= addedEvent.hour.length; i++) {
-  console.log(`You have ${addedEvent[i]} at ${time}`)
+  console.log(`You have ${addedEvent.event} at ${time}`)
   if (eventBlock === null) {
     console.log(`You are free at ${time}!`);
   }
 }
 
+// TIME BLOCK ?
+var time = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+//want typed info to display within input element
+// var inputData = $("<input>").text
+
+//want time to be defined as the event hour's indexNumber
+// var time = addedEvent.hour[0];
+
+
+
+// CURRENT TIME
+var currentTime = moment().hour();
+time = moment().format("h:mm A");
+console.log(time);
+
+//color code events based on relation to current time
+function colorCodeEvent(){
+if (addedEvent.hour = currentTime) {
+  console.log("present" + $(".present"));
+} else if (addedEvent.hour < currentTime){
+  console.log("past" + $(".past"));
+  } else (addedEvent.hour > currentTime) 
+    console.log("future" + $(".future"));
+}
+
+// line 19 time("hh:mm A") ?
 
 
 
 
 
-// function displaySchedule() {}
 
 
-//   //mySchedule refers to the current day's schedule
-// var Event1 = [];
-// var eventData = $("input")
-// $("input").text(`${Event1}`)
 
-// console.log(eventData);
+var saveBtn = $(".saveBtn");
+// var saveEvent = $(".saveBtn");
+
+// // function to save newest schedule and store it
+// function saveSchedule() {
+//   localStorage.setItem("savedSchedule");JSON.stringify(savedSchedule)
+
+// // on save button want to save event
+//   saveEvent.on("click", function(event) {
+//   event.preventDefault();
+//   localStorage.setItem(hour, addedEvent)
+//   console.log(saveSchedule);
+//   }
+//   )
+// }
 
 
-// var currentTime = moment().hour();
-// function displayAddedEvents(){};
-// for (i = 9; i <=19; i++)
+// // condition to display past, present, future color-code for events
+// // if (time < moment()) {
+// //   addedEvent === $(`<div class="past"></div>`),
+// // } else if (time === moment()) {
+// //   addedEvent === $(".present")
+  
+// // } else if (time === moment()) {
+// //   addedEvent === $(".future")
+// // }
 
-//   $(".container").append(Event1)
 
-//   let schedule = JSON.parse(localStorage.getItem("schedule"));
+
+
+// // function displaySchedule() {}
+
+
+// //   //mySchedule refers to the current day's schedule
+// // var Event1 = [];
+// // var eventData = $("input")
+// // $("input").text(`${Event1}`)
+
+// // console.log(eventData);
+
+
+// // var currentTime = moment().hour();
+// // function displayAddedEvents(){};
+// // for (i = 9; i <=19; i++)
+
+// //   $(".container").append(Event1)
+
+// //   let schedule = JSON.parse(localStorage.getItem("schedule"));
 
 
 
@@ -75,30 +111,31 @@ function getHeaderDate() {
 // load today's date (header date) [correct]
 getHeaderDate();
 
-// const hour = moment().format("h a");
-// // var addedEvent = $()
+// // const hour = moment().format("h a");
+// // // var addedEvent = $()
 
-// function getTime() {
-//   // var currentTime = moment().hour(9);
-//   for (let hour = 9; hour <= 19; hour++) {
-//   // console.log(currentTime);
-//   console.log(hour);
-// }
-// // let hour = i;
+// // function getTime() {
+// //   // var currentTime = moment().hour(9);
+// //   for (let hour = 9; hour <= 19; hour++) {
+// //   // console.log(currentTime);
+// //   console.log(hour);
+// // }
+// // // let hour = i;
 
-// // var hour = moment().hour();
-// // let time = moment().format("h A");
-// }
-// getTime(9);
+// var time = moment().hour();
+// time = moment().format("h A");
+// console.log(time);
+// // }
+// // getTime(9);
 
-// let container = $(".container");
+// // let container = $(".container");
 
-// let hour9Div = $(`<div class='row time-block'>
-//   <div class='col-2 hour'>${hour}</div>
-//   <textarea class="col-8 description" placeholder="add event"></textarea> 
-//   <button class="col-2 saveBtn>Save</button>
-//   </div>`);
-// container.append(hour9Div);
+// // let hour9Div = $(`<div class='row time-block'>
+// //   <div class='col-2 hour'>${hour}</div>
+// //   <textarea class="col-8 description" placeholder="add event"></textarea> 
+// //   <button class="col-2 saveBtn>Save</button>
+// //   </div>`);
+// // container.append(hour9Div);
 
 
 
